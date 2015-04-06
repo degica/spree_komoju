b333472be9f59666c351f538764d0c85fab28813
camelmasa <camelmasa@gmail.com>
2015-04-06 18:05:32 +0900

    Create Spree::KomojuGateway class

---
+++ app/models/spree/komoju_gateway.rb
@@ -0,0 +1,40 @@
+module Spree
+  class KomojuGateway < Gateway
+    preference :api_key, :string
+
+    def provider_class
+      ActiveMerchant::Billing::KomojuGateway
+    end
+
+    def options
+      super.merge(login: preferred_api_key, test: preferred_test_mode)
+    end
+
+    def auto_capture?
+      false
+    end
+
+    def supports?(source)
+      true
+    end
+
+    # We need to change price from cents to dollar for Komoju gateway.
+    # Because, Komoju gateway supports JPY currency only.
+    def change_options_to_dollar(options)
+      %i(shipping tax subtotal discount).each { |key| options[key] = options[key] / 100.0 }
+      options
+    end
+
+    def gateway_type
+      self.class.to_s.split("::Komoju").last.underscore
+    end
+
+    def method_type
+      "komoju_#{gateway_type}"
+    end
+
+    def payment_source_class
+      "Spree::#{gateway_type.camelcase}".constantize
+    end
+  end
+end
