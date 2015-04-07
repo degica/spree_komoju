5bbb18fc26526f9be2d3dc338c39cc983d830683
camelmasa <camelmasa@gmail.com>
2015-04-07 16:23:24 +0900

    Add permitted params for pay easy

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,6 +2,7 @@ Spree::CheckoutController.class_eval do
   def permitted_source_attributes
     super.push(permitted_komoju_konbini_attributes)
     super.push(permitted_komoju_banktransfer_attributes)
+    super.push(permitted_komoju_pay_easy_attributes)
     super.flatten
   end
 
@@ -12,6 +13,10 @@ Spree::CheckoutController.class_eval do
   end
 
   def permitted_komoju_banktransfer_attributes
+    [:email, :phone, :family_name, :given_name, :family_name_kana, :given_name_kana]
+  end
+
+  def permitted_komoju_pay_easy_attributes
     [:email, :phone, :family_name, :given_name, :family_name_kana, :given_name_kana]
   end
 end
