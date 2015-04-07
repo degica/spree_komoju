4b8924ae670b78dbf5b1e6605bb1d072b18b740d
camelmasa <camelmasa@gmail.com>
2015-04-07 17:14:29 +0900

    Add tests

---
+++ spec/models/spree/gateway/komoju_pay_easy_spec.rb
@@ -0,0 +1,58 @@
+require "spec_helper"
+
+describe Spree::Gateway::KomojuPayEasy, type: :model do
+  subject { described_class.new }
+
+  describe "#authorize" do
+    let(:money) { 1000.0 }
+    let(:source) do
+      Spree::PayEasy.create!(
+        email: "email",
+        given_name: "given_name",
+        family_name: "family_name",
+        given_name_kana: "given_name_kana",
+        family_name_kana: "family_name_kana"
+      )
+    end
+    let(:options) { { login: "api_key", shipping: 100.0, tax: 200.0, subtotal: 800.0, discount: 100.0,
+                      currency: currency } }
+    let(:details) do
+      {
+        type: "pay_easy",
+        phone: nil,
+        email: "email",
+        given_name: "given_name",
+        family_name: "family_name",
+        given_name_kana: "given_name_kana",
+        family_name_kana: "family_name_kana"
+      }
+    end
+    let(:params) { { "payment_deadline" => Time.now.iso8601.to_s, "payment_details" => { "pay_url" => "pay_url", "instructions_url" => "instructions_url" } } }
+    let(:response) { double(ActiveMerchant::Billing::Response, params: params) }
+
+    before do
+      allow_any_instance_of(Spree::Gateway::KomojuPayEasy).to receive(:options) { options }
+    end
+
+    context "with currency is USD" do
+      let(:currency) { "USD" }
+
+      it "calls ActiveMerchant::Billing::KomojuGateway#purchase" do
+        expect_any_instance_of(ActiveMerchant::Billing::KomojuGateway).to receive(:purchase).with(800.0, details, options) { response }
+
+        subject.authorize(money, source, options)
+      end
+    end
+
+    context "with currency is JPY" do
+      let(:currency) { "JPY" }
+
+      it "calls ActiveMerchant::Billing::KomojuGateway#purchase" do
+        changed_option =  { login: "api_key", shipping: 1.0, tax: 2.0, subtotal: 8.0, discount: 1.0, currency: currency }
+        expect_any_instance_of(ActiveMerchant::Billing::KomojuGateway).to receive(:purchase).with(998.0, details, changed_option) { response }
+
+        subject.authorize(money, source, options)
+      end
+    end
+  end
+end
---
+++ spec/models/spree/pay_easy_spec.rb
@@ -0,0 +1,69 @@
+require "spec_helper"
+
+describe Spree::PayEasy, type: :model do
+  subject { described_class.new }
+
+  describe "#actions" do
+    it { expect(subject.actions).to eq ["capture", "void"] }
+  end
+
+  describe "#can_capture?" do
+    let(:payment) { double(Spree::Payment, state: state) }
+
+    before do
+      allow(subject).to receive(:payment) { payment }
+    end
+
+    context "when payment state is not checkout or pending" do
+      let(:state) { "void" }
+
+      it { expect(subject.can_capture?(payment)).to be_falsy }
+    end
+
+    context "when payment state is pending" do
+      let(:payment) { double(Spree::Payment, state: state, source: source) }
+      let(:state) { "pending" }
+      let(:source) { double(Spree::BankTransfer, expires_at: expires_at) }
+
+      before do
+        allow(subject).to receive(:payment) { payment }
+      end
+
+      context "when expires_at is tomorrow" do
+        let(:expires_at) { Date.tomorrow }
+
+        it { expect(subject.can_capture?(payment)).to be_truthy }
+      end
+
+      context "when expires_at is yesterday" do
+        let(:expires_at) { Date.yesterday}
+
+        it { expect(subject.can_capture?(payment)).to be_falsy }
+      end
+    end
+  end
+
+  describe "#can_void?" do
+    let(:payment) { double(Spree::Payment, state: state) }
+
+    before do
+      allow(subject).to receive(:payment) { payment }
+    end
+
+    context "when payment state is void" do
+      let(:state) { "void" }
+
+      it { expect(subject.can_void?(payment)).to be_falsy }
+    end
+
+    context "when payment state is pending" do
+      let(:state) { "pending" }
+
+      it { expect(subject.can_void?(payment)).to be_truthy }
+    end
+  end
+
+  describe "#instruction_partial_path" do
+    it { expect(subject.instruction_partial_path).to eq "spree/orders/pay_easy" }
+  end
+end
