42aa7485d61c8cb33f0d7fe94b08c1fe34ce6e99
camelmasa <camelmasa@gmail.com>
2015-04-08 17:13:21 +0900

    Add permitted params for web money

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -3,6 +3,7 @@ Spree::CheckoutController.class_eval do
     super.push(permitted_komoju_konbini_attributes)
     super.push(permitted_komoju_banktransfer_attributes)
     super.push(permitted_komoju_pay_easy_attributes)
+    super.push(permitted_komoju_web_money_attributes)
     super.flatten.uniq
   end
 
@@ -19,5 +20,8 @@ Spree::CheckoutController.class_eval do
   def permitted_komoju_pay_easy_attributes
     [:email, :phone, :family_name, :given_name, :family_name_kana, :given_name_kana]
   end
-end
 
+  def permitted_komoju_web_money_attributes
+    [:prepaid_number]
+  end
+end
