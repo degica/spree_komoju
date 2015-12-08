5104ca9c45f597e8f7a700e99224aca9e00a7732
Richard Ramsden <richard@rramsden.ca>
2015-12-08 17:10:23 +0900

    Remove profile_id strings from database

--- db/migrate/20151126050103_add_gateway_profile_ids_to_spree_konbinis.rb
+++
@@ -1,6 +0,0 @@
-class AddGatewayProfileIdsToSpreeKonbinis < ActiveRecord::Migration
-  def change
-    add_column :spree_konbinis, :gateway_customer_profile_id, :string
-    add_column :spree_konbinis, :gateway_payment_profile_id, :string
-  end
-end
