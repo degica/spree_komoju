7965493997e967dc253eb10c06980ff86c53535b
Richard Ramsden <richard@rramsden.ca>
2015-12-16 09:36:41 +0900

    Extract controller helper

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,27 +1,3 @@
 Spree::CheckoutController.class_eval do
-  def permitted_source_attributes
-    super.push(permitted_komoju_konbini_attributes)
-    super.push(permitted_komoju_banktransfer_attributes)
-    super.push(permitted_komoju_pay_easy_attributes)
-    super.push(permitted_komoju_web_money_attributes)
-    super.flatten.uniq
-  end
-
-  private
-
-  def permitted_komoju_konbini_attributes
-    :convenience
-  end
-
-  def permitted_komoju_banktransfer_attributes
-    [:email, :phone, :family_name, :given_name, :family_name_kana, :given_name_kana]
-  end
-
-  def permitted_komoju_pay_easy_attributes
-    [:email, :phone, :family_name, :given_name, :family_name_kana, :given_name_kana]
-  end
-
-  def permitted_komoju_web_money_attributes
-    [:email, :prepaid_number]
-  end
+  include SpreeKomoju::ControllerHelpers
 end
--- lib/spree_komoju.rb
+++ lib/spree_komoju.rb
@@ -1,6 +1,7 @@
 require 'spree_core'
 require 'spree_komoju/engine'
 require 'spree_komoju/errors'
+require 'spree_komoju/controller_helpers'
 
 # This extension adds HTTP PATCH to ssl_request.
 # This is needed for WebMoney multi-card in Komoju.
